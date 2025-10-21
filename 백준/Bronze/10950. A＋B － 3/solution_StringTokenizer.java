6985c1e545bdebe8fbc7bfcfcef9cef2ef978490
Eunsu Kim <eunsue1222@gmail.com>
2025-10-21 17:53:24 +0900

    [Bronze V] Title: A+B - 3, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/solution_StringTokenizer.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 수
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            System.out.println(A + B);
+        }
+    }
+}
