ab8fe8d0ed98eb6bd1c1cf23f70dd09e2230c9df
Eunsu Kim <46877867+eunsue1222@users.noreply.github.com>
2025-10-22 23:32:58 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 164 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -5,16 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
         
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= N - i; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 1; k <= i; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
+            sb.append(" ".repeat(N - i));
+            sb.append("*".repeat(i));
+            sb.append("\n");
         }
 
+        System.out.print(sb);
         sc.close();
     }
 }
