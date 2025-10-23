2394917e1c6844b4ec1474f93eccc45a5896b9a1
Eunsu Kim <eunsue1222@gmail.com>
2025-10-23 11:33:56 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 75 ms, Memory: 22,400 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/solution_BufferedReader.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+         
+        for (int tc = 1; tc <= T; tc++) {
+            String[] input = br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+             
+            System.out.print("#" + tc + " ");
+             
+            if (A > B) {
+                System.out.println(">");
+            } else if (A < B) {
+                System.out.println("<");
+            } else {
+                System.out.println("=");
+            }
+        }
+    }
+}
