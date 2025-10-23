dd676b579485c8e0312c6764753ff4b1d2239e24
Eunsu Kim <46877867+eunsue1222@users.noreply.github.com>
2025-10-23 11:29:25 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 75 ms, Memory: 22,400 KB -BaekjoonHub

--- SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -1,13 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
         
         for (int tc = 1; tc <= T; tc++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
+            String[] input = br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
             
             System.out.print("#" + tc + " ");
             
@@ -19,6 +22,5 @@ public class Solution {
                 System.out.println("=");
             }
         }
-        sc.close();
     }
 }
